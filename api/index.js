1eeb3bb537e9bb8f30ee3a0404558d13ddb1e3a8
sarmadhbokhari@gmail.com <sarmadhbokhari@gmail.com>
2019-06-08 18:19:25 -0700

    Cleanup api

--- api/index.js
+++ api/index.js
@@ -1,18 +1,40 @@
-var app = require("express")();
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
-var fs = require("fs");
-var events = JSON.parse(fs.readFileSync("./challenge_data.json", "utf8"));
+const fs = require("fs");
+const events = JSON.parse(fs.readFileSync("./challenge_data.json", "utf8"));
 
-io.on("connection", function(socket) {
+// const sortedEvents = events.sort((a, b) => a.sent_at_second - b.sent_at_second);
+
+// function sleep(duration) {
+//   return new Promise(resolve => setTimeout(resolve, duration));
+// }
+
+io.on("connection", async function(socket) {
   console.log("a user connected");
 
   socket.on("disconnect", function() {
     console.log("user disconnected");
   });
 
+  // let ticker = 0;
+  // while (sortedEvents.length) {
+  //   for (let i = 0; i < sortedEvents.length; i++) {
+  //     if (sortedEvents[i].sent_at_second === ticker) {
+  //       io.emit("new_event", JSON.stringify(sortedEvents[i]));
+  //       sortedEvents.splice(0, 1);
+  //     } else {
+  //       break;
+  //     }
+  //   }
+
+  //   await sleep(1000);
+  //   ticker++;
+  // }
+
   let currentIndex = 0;
+
   const interval = setInterval(() => {
     const currentEvents = events.filter(
       event => event.sent_at_second === currentIndex
@@ -20,6 +42,11 @@ io.on("connection", function(socket) {
     currentEvents.forEach(event => io.emit("new_event", JSON.stringify(event)));
 
     currentIndex++;
+
+    // clearInterval after all events have been processed
+    if (currentIndex === events.length) {
+      clearInterval(interval);
+    }
   }, 1000);
 });
 
